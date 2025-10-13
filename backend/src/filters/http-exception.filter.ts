67ed03df30aa33606fdf4c9b0c22930bd9a85415
Caio-C8 <caiomgf50@gmail.com>
2025-10-13 20:21:32 -0300

    adiciona interceptores e filtros para formatar as respostas da API

---
+++ backend/src/filters/http-exception.filter.ts
@@ -0,0 +1,54 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  BadRequestException,
+} from "@nestjs/common";
+import { Response } from "express";
+import { Resposta } from "../utils/resposta-api";
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    if (exception instanceof HttpException) {
+      const status = exception.getStatus();
+      const exceptionResponse = exception.getResponse();
+
+      if (
+        exception instanceof BadRequestException &&
+        typeof exceptionResponse === "object" &&
+        "message" in exceptionResponse &&
+        Array.isArray(exceptionResponse.message)
+      ) {
+        const validationErrors = exceptionResponse.message as string[];
+        response
+          .status(status)
+          .json(Resposta.erro("Erro de validação.", validationErrors, status));
+        return;
+      }
+
+      const errorMessage =
+        typeof exceptionResponse === "string"
+          ? exceptionResponse
+          : (exceptionResponse as any).message || "Erro no servidor";
+      response.status(status).json(Resposta.erro(errorMessage, [], status));
+    } else {
+      console.error("Erro não tratado:", exception);
+
+      response
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .json(
+          Resposta.erro(
+            "Ocorreu um erro interno no servidor.",
+            [],
+            HttpStatus.INTERNAL_SERVER_ERROR
+          )
+        );
+    }
+  }
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./filters/http-exception.filter";
+import { RespostaInterceptor } from "./interceptors/resposta.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new RespostaInterceptor());
 
   app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
-// "postinstall": "npx prisma generate" // package.json
