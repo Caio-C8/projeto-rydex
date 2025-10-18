f2ff89990a665b301bce755dd8af874f0756eb96
Caio-C8 <caiomgf50@gmail.com>
2025-10-18 18:01:43 -0300

    altera forma para salvar imagens e retornar o entregador

--- backend/src/entregadores/entregadores.controller.ts
+++ backend/src/entregadores/entregadores.controller.ts
@@ -10,9 +10,9 @@ import {
   UseInterceptors,
 } from "@nestjs/common";
 import { EntregadoresService } from "./entregadores.service";
-import { FilesInterceptor } from "@nestjs/platform-express";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { Entregador, Arquivos } from "@prisma/client";
 import { CriarEntregadorDto } from "./dto/criar-entregador.dto";
-import { Entregador } from "@prisma/client";
 import { AlterarEntregadorDto } from "./dto/alterar-entregador.dto";
 import { RespostaImagemDto } from "./dto/resposta-imagem.dto";
 
@@ -20,48 +20,64 @@ import { RespostaImagemDto } from "./dto/resposta-imagem.dto";
 export class EntregadoresController {
   constructor(private readonly entregadoresService: EntregadoresService) {}
 
-  @Get()
-  buscarEntregadores(): Promise<Entregador[]> {
-    return this.entregadoresService.buscarEntregadores();
-  }
+  // @Get()
+  // buscarEntregadores(): Promise<Entregador[]> {
+  //   return this.entregadoresService.buscarEntregadores();
+  // }
 
-  @Get(":id")
-  buscarEntregador(
-    @Param("id", ParseIntPipe) id: number
-  ): Promise<Entregador | null> {
-    return this.entregadoresService.buscarEntregador(id);
-  }
+  // @Get(":id")
+  // buscarEntregador(
+  //   @Param("id", ParseIntPipe) id: number
+  // ): Promise<Entregador | null> {
+  //   return this.entregadoresService.buscarEntregador(id);
+  // }
 
-  @Get(":id/imagens")
-  buscarImagens(
-    @Param("id", ParseIntPipe) id: number
-  ): Promise<RespostaImagemDto[]> {
-    return this.entregadoresService.buscarImagens(id);
-  }
+  // @Get(":id/imagens")
+  // buscarImagens(
+  //   @Param("id", ParseIntPipe) id: number
+  // ): Promise<RespostaImagemDto[]> {
+  //   return this.entregadoresService.buscarImagens(id);
+  // }
 
   @Post()
-  @UseInterceptors(FilesInterceptor("imagens"))
+  // 1. Use o FileFieldsInterceptor
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: "imagemCnh", maxCount: 1 },
+      { name: "imagemDocVeiculo", maxCount: 1 },
+    ])
+  )
   criarEntregador(
     @Body() criarEntregadorDto: CriarEntregadorDto,
-    @UploadedFiles() imagens: Array<Express.Multer.File>
-  ): Promise<Entregador> {
+    @UploadedFiles()
+    files: {
+      imagemCnh?: Express.Multer.File[];
+      imagemDocVeiculo?: Express.Multer.File[];
+    }
+  ): Promise<Entregador & { arquivos: Arquivos[] }> {
+    const imagemCnh = files.imagemCnh ? files.imagemCnh[0] : undefined;
+    const imagemDocVeiculo = files.imagemDocVeiculo
+      ? files.imagemDocVeiculo[0]
+      : undefined;
+
     return this.entregadoresService.criarEntregador(
       criarEntregadorDto,
-      imagens
+      imagemCnh,
+      imagemDocVeiculo
     );
   }
 
-  @Patch(":id")
-  @UseInterceptors(FilesInterceptor("imagens"))
-  atualizarEntregador(
-    @Param("id", ParseIntPipe) id: number,
-    @Body() alterarEntregadorDto: AlterarEntregadorDto,
-    @UploadedFiles() imagens: Array<Express.Multer.File>
-  ): Promise<Entregador> {
-    return this.entregadoresService.alterarEntregador(
-      id,
-      alterarEntregadorDto,
-      imagens
-    );
-  }
+  // @Patch(":id")
+  // @UseInterceptors(FilesInterceptor("imagens"))
+  // atualizarEntregador(
+  //   @Param("id", ParseIntPipe) id: number,
+  //   @Body() alterarEntregadorDto: AlterarEntregadorDto,
+  //   @UploadedFiles() imagens: Array<Express.Multer.File>
+  // ): Promise<Entregador> {
+  //   return this.entregadoresService.alterarEntregador(
+  //     id,
+  //     alterarEntregadorDto,
+  //     imagens
+  //   );
+  // }
 }
--- backend/src/entregadores/entregadores.service.ts
+++ backend/src/entregadores/entregadores.service.ts
@@ -5,8 +5,13 @@ import {
 } from "@nestjs/common";
 import { PrismaService } from "src/prisma.service";
 import { PrismaClient } from "@prisma/client";
+import { Entregador, Arquivos, Prisma } from "@prisma/client";
+
+import * as fs from "fs/promises";
+import * as path from "path";
+import { posix } from "path";
+
 import { CriarEntregadorDto } from "./dto/criar-entregador.dto";
-import { Entregador } from "@prisma/client";
 import { AlterarEntregadorDto } from "./dto/alterar-entregador.dto";
 import { RespostaImagemDto } from "./dto/resposta-imagem.dto";
 
@@ -14,51 +19,52 @@ import { RespostaImagemDto } from "./dto/resposta-imagem.dto";
 export class EntregadoresService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async buscarEntregadores(): Promise<Entregador[]> {
-    return await this.prisma.entregador.findMany();
-  }
+  // async buscarEntregadores(): Promise<Entregador[]> {
+  //   return await this.prisma.entregador.findMany();
+  // }
 
-  async buscarEntregador(id: number): Promise<Entregador> {
-    const entregador = await this.prisma.entregador.findUnique({
-      where: {
-        id: id,
-      },
-    });
+  // async buscarEntregador(id: number): Promise<Entregador> {
+  //   const entregador = await this.prisma.entregador.findUnique({
+  //     where: {
+  //       id: id,
+  //     },
+  //   });
 
-    if (!entregador) {
-      throw new NotFoundException(`Entregador com o ID ${id} não encontrado.`);
-    }
+  //   if (!entregador) {
+  //     throw new NotFoundException(`Entregador com o ID ${id} não encontrado.`);
+  //   }
 
-    return entregador;
-  }
+  //   return entregador;
+  // }
 
-  async buscarImagens(id: number): Promise<RespostaImagemDto[]> {
-    const imagensDoBanco = await this.prisma.imagens.findMany({
-      where: {
-        entregador_id: id,
-      },
-    });
+  // async buscarImagens(id: number): Promise<RespostaImagemDto[]> {
+  //   const imagensDoBanco = await this.prisma.imagens.findMany({
+  //     where: {
+  //       entregador_id: id,
+  //     },
+  //   });
 
-    const imagensFormatadas: RespostaImagemDto[] = imagensDoBanco.map(
-      (imagem) => {
-        return {
-          id: imagem.id,
-          nome_imagem: imagem.nome_imagem,
-          conteudo: imagem.conteudo
-            ? Buffer.from(imagem.conteudo).toString("base64")
-            : null,
-          entregador_id: imagem.entregador_id,
-        };
-      }
-    );
+  //   const imagensFormatadas: RespostaImagemDto[] = imagensDoBanco.map(
+  //     (imagem) => {
+  //       return {
+  //         id: imagem.id,
+  //         nome_imagem: imagem.nome_imagem,
+  //         conteudo: imagem.conteudo
+  //           ? Buffer.from(imagem.conteudo).toString("base64")
+  //           : null,
+  //         entregador_id: imagem.entregador_id,
+  //       };
+  //     }
+  //   );
 
-    return imagensFormatadas;
-  }
+  //   return imagensFormatadas;
+  // }
 
   async criarEntregador(
     criarEntregadorDto: CriarEntregadorDto,
-    imagens: Array<Express.Multer.File>
-  ): Promise<Entregador> {
+    imagemCnh?: Express.Multer.File,
+    imagemDocVeiculo?: Express.Multer.File
+  ): Promise<Entregador & { arquivos: Arquivos[] }> {
     return this.prisma.$transaction(async (prisma) => {
       const novoEntregador = await prisma.entregador.create({
         data: {
@@ -73,115 +79,166 @@ export class EntregadoresService {
         },
       });
 
-      if (imagens && imagens.length > 0) {
-        for (const imagem of imagens) {
-          await prisma.imagens.create({
-            data: {
-              entregador_id: novoEntregador.id,
-              nome_imagem: imagem.originalname,
-              conteudo: imagem.buffer,
-            },
-          });
-        }
+      if (imagemCnh) {
+        await this.salvarArquivo(prisma, novoEntregador, imagemCnh, "cnh");
       }
 
-      return novoEntregador;
+      if (imagemDocVeiculo) {
+        await this.salvarArquivo(
+          prisma,
+          novoEntregador,
+          imagemDocVeiculo,
+          "docVeiculo"
+        );
+      }
+
+      const entregadorCompleto = await prisma.entregador.findUnique({
+        where: { id: novoEntregador.id },
+        include: {
+          arquivos: true,
+        },
+      });
+
+      if (!entregadorCompleto) {
+        throw new Error("Falha ao buscar entregador recém-criado.");
+      }
+
+      return entregadorCompleto;
     });
   }
 
-  async alterarEntregador(
-    id: number,
-    alterarEntregadorDto: AlterarEntregadorDto,
-    imagens: Array<Express.Multer.File>
-  ): Promise<Entregador> {
-    return this.prisma.$transaction(async (prisma) => {
-      const entregadorExistente = await prisma.entregador.findUnique({
-        where: { id },
-      });
+  private async salvarArquivo(
+    prisma: Prisma.TransactionClient,
+    entregador: Entregador,
+    arquivo: Express.Multer.File,
+    tipoArquivo: string
+  ): Promise<Arquivos> {
+    const diretorioDestinoFs = path.join("uploads", "entregadores");
 
-      if (!entregadorExistente) {
-        throw new NotFoundException(
-          `Entregador com o ID ${id} não encontrado.`
-        );
-      }
+    const diretorioDestinoUrl = "entregadores";
 
-      const { idsImagensParaSubstituir, ...dadosEntregador } =
-        alterarEntregadorDto;
-      const entregadorAtualizado = await prisma.entregador.update({
-        where: { id },
-        data: {
-          ...dadosEntregador,
-          placa_veiculo: alterarEntregadorDto.placaVeiculo,
-          data_nascimento: alterarEntregadorDto.dataNascimento
-            ? new Date(alterarEntregadorDto.dataNascimento)
-            : undefined,
-        },
-      });
+    await fs.mkdir(diretorioDestinoFs, { recursive: true });
 
-      if (imagens && imagens.length > 0) {
-        await this.processarAtualizacaoImagens(
-          id,
-          imagens,
-          idsImagensParaSubstituir,
-          prisma
-        );
-      }
+    const nomeSanitizado = entregador.nome
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/\p{M}/gu, "")
+      .replace(/[^a-z0-9]/g, " ")
+      .trim()
+      .replace(/\s+/g, "_");
 
-      return entregadorAtualizado;
+    const extensao = path.extname(arquivo.originalname);
+
+    const nomeArquivo = `${tipoArquivo}_${entregador.id}_${nomeSanitizado}_${Date.now()}${extensao}`;
+
+    const caminhoCompletoFs = path.join(diretorioDestinoFs, nomeArquivo);
+
+    const caminhoUrlDb = posix.join(diretorioDestinoUrl, nomeArquivo);
+
+    await fs.writeFile(caminhoCompletoFs, arquivo.buffer);
+
+    return prisma.arquivos.create({
+      data: {
+        nome: nomeArquivo,
+        caminho: caminhoUrlDb,
+        entregador_id: entregador.id,
+      },
     });
   }
 
-  private async processarAtualizacaoImagens(
-    entregadorId: number,
-    imagens: Array<Express.Multer.File>,
-    idsImagensParaSubstituir: string | undefined,
-    prisma: Omit<
-      PrismaClient,
-      "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends"
-    >
-  ) {
-    if (!idsImagensParaSubstituir) {
-      throw new BadRequestException("Imagens não localizadas.");
-    }
+  // async alterarEntregador(
+  //   id: number,
+  //   alterarEntregadorDto: AlterarEntregadorDto,
+  //   imagens: Array<Express.Multer.File>
+  // ): Promise<Entregador> {
+  //   return this.prisma.$transaction(async (prisma) => {
+  //     const entregadorExistente = await prisma.entregador.findUnique({
+  //       where: { id },
+  //     });
 
-    const idsParaSubstituir = idsImagensParaSubstituir
-      .split(",")
-      .map((idStr) => parseInt(idStr.trim(), 10));
+  //     if (!entregadorExistente) {
+  //       throw new NotFoundException(
+  //         `Entregador com o ID ${id} não encontrado.`
+  //       );
+  //     }
 
-    if (idsParaSubstituir.some(isNaN)) {
-      throw new BadRequestException("Erro ao processar dados.");
-    }
+  //     const { idsImagensParaSubstituir, ...dadosEntregador } =
+  //       alterarEntregadorDto;
+  //     const entregadorAtualizado = await prisma.entregador.update({
+  //       where: { id },
+  //       data: {
+  //         ...dadosEntregador,
+  //         placa_veiculo: alterarEntregadorDto.placaVeiculo,
+  //         data_nascimento: alterarEntregadorDto.dataNascimento
+  //           ? new Date(alterarEntregadorDto.dataNascimento)
+  //           : undefined,
+  //       },
+  //     });
 
-    if (idsParaSubstituir.length !== imagens.length) {
-      throw new BadRequestException(
-        "O número de imagens fornecidas não corresponde ao número de imagens enviadas."
-      );
-    }
+  //     if (imagens && imagens.length > 0) {
+  //       await this.processarAtualizacaoImagens(
+  //         id,
+  //         imagens,
+  //         idsImagensParaSubstituir,
+  //         prisma
+  //       );
+  //     }
 
-    for (let i = 0; i < idsParaSubstituir.length; i++) {
-      const idImagem = idsParaSubstituir[i];
-      const novaImagem = imagens[i];
+  //     return entregadorAtualizado;
+  //   });
+  // }
 
-      const imagemExistente = await prisma.imagens.findFirst({
-        where: {
-          id: idImagem,
-          entregador_id: entregadorId,
-        },
-      });
+  // // private async processarAtualizacaoImagens(
+  // //   entregadorId: number,
+  // //   imagens: Array<Express.Multer.File>,
+  // //   idsImagensParaSubstituir: string | undefined,
+  // //   prisma: Omit<
+  // //     PrismaClient,
+  // //     "$connect" | "$disconnect" | "$on" | "$transaction" | "$use" | "$extends"
+  // //   >
+  // // ) {
+  // //   if (!idsImagensParaSubstituir) {
+  // //     throw new BadRequestException("Imagens não localizadas.");
+  // //   }
 
-      if (!imagemExistente) {
-        throw new NotFoundException(
-          `A imagem com ID ${idImagem} não foi encontrada ou não pertence a este entregador.`
-        );
-      }
+  // //   const idsParaSubstituir = idsImagensParaSubstituir
+  // //     .split(",")
+  // //     .map((idStr) => parseInt(idStr.trim(), 10));
 
-      await prisma.imagens.update({
-        where: { id: idImagem },
-        data: {
-          conteudo: novaImagem.buffer,
-          nome_imagem: novaImagem.originalname,
-        },
-      });
-    }
-  }
+  // //   if (idsParaSubstituir.some(isNaN)) {
+  // //     throw new BadRequestException("Erro ao processar dados.");
+  // //   }
+
+  // //   if (idsParaSubstituir.length !== imagens.length) {
+  // //     throw new BadRequestException(
+  // //       "O número de imagens fornecidas não corresponde ao número de imagens enviadas."
+  // //     );
+  // //   }
+
+  // //   for (let i = 0; i < idsParaSubstituir.length; i++) {
+  // //     const idImagem = idsParaSubstituir[i];
+  // //     const novaImagem = imagens[i];
+
+  // //     const imagemExistente = await prisma.imagens.findFirst({
+  // //       where: {
+  // //         id: idImagem,
+  // //         entregador_id: entregadorId,
+  // //       },
+  // //     });
+
+  // //     if (!imagemExistente) {
+  // //       throw new NotFoundException(
+  // //         `A imagem com ID ${idImagem} não foi encontrada ou não pertence a este entregador.`
+  // //       );
+  // //     }
+
+  // //     await prisma.imagens.update({
+  // //       where: { id: idImagem },
+  // //       data: {
+  // //         conteudo: novaImagem.buffer,
+  // //         nome_imagem: novaImagem.originalname,
+  // //       },
+  // //     });
+  // //   }
+  // }
 }
