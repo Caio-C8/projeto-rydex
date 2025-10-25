b98f2cbfb0177f71d5012a9fd5afcaaa297803d0
DevArthurSousa <arthursilva7383m@gmail.com>
2025-10-25 10:18:48 -0300

    rota para buscar todas es empresas

--- backend/src/empresas/empresas.controller.ts
+++ backend/src/empresas/empresas.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Get } from "@nestjs/common";
 import { EmpresasServices } from "./empresas.service";
 import { CriarEmpresaDto } from "./dto/criar-empresa.dto";
 import { Empresa } from "@prisma/client";
+import { promises } from "dns";
 
 @Controller("empresas")
 export class EmpresasController {
   constructor(private readonly empresasService: EmpresasServices) {}
+
+  @Get()
+  buscarEmpresas(): Promise<Empresa[]> {
+    return this.empresasService.buscarEmpresas();
+  }
 
   @Post()
   criarEmrpesa(@Body() criarEmpresaDto: CriarEmpresaDto): Promise<Empresa> {
--- backend/src/empresas/empresas.service.ts
+++ backend/src/empresas/empresas.service.ts
@@ -7,6 +7,10 @@ import { Empresa } from "@prisma/client";
 export class EmpresasServices {
   constructor(private readonly prismaService: PrismaService) {}
 
+  async buscarEmpresas(): Promise<Empresa[]> {
+    return await this.prismaService.empresa.findMany();
+  }
+
   async criarEmpresa(criarEmpresaDto: CriarEmpresaDto): Promise<Empresa> {
     return await this.prismaService.empresa.create({
       data: criarEmpresaDto,
