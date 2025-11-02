2a7817cee1c66a002d94a3b9e6052a5bed0741a9
Higor004 <higorhonoriosilva@outlook.com>
2025-11-02 16:35:58 -0300

    Correcao do componente botao

--- web/src/components/ui/Input/Input.tsx
+++ web/src/components/ui/Input/Input.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import "./Input.css";
 
-// Interface para as propriedades do Input
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  label?: string;
+  Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  desativado?: boolean;
+  onIconClick?: () => void;
+  iconPosition?: "left" | "right";
+  mostrarIcone?: boolean;
+};
 
-const Input = (props: InputProps) => {
-  return <input className="input" {...props} />;
+const Input = ({
+  label,
+  Icon,
+  desativado = false,
+  onIconClick,
+  iconPosition = "left",
+  mostrarIcone = true,
+  ...props
+}: InputProps) => {
+  return (
+    <div className="input-container">
+      {label && <label className="input-label">{label}</label>}
+      <div className="input-wrapper">
+        {Icon && mostrarIcone && (
+          <Icon
+            className={`input-icon ${iconPosition}`}
+            onClick={onIconClick}
+          />
+        )}
+        <input
+          className={`input ${Icon ? `with-icon ${iconPosition}` : ""}`}
+          {...props}
+          disabled={desativado}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Input;
