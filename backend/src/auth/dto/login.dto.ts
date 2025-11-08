8f0b6790af73d3a57a2dbf21948ba902da5ea82c
Caio-C8 <caiomgf50@gmail.com>
2025-11-08 15:49:57 -0300

    adiciona dto de login

---
+++ backend/src/auth/dto/login.dto.ts
@@ -0,0 +1,54 @@
+import {
+  IsDateString,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  Matches,
+  IsNumber,
+  IsOptional,
+  IsEnum,
+} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Transform, Type } from "class-transformer";
+import { IsCpf } from "../../validators/is-cpf.validator";
+import { IsCelular } from "src/validators/is-celular.validator";
+
+export enum TipoUsuario {
+  EMPRESA = "empresa",
+  ENTREGADOR = "entregador",
+}
+
+export class LoginDto {
+  @ApiProperty({
+    description: "Email único para login",
+    example: "joao.silva@email.com",
+  })
+  @IsEmail({}, { message: "E-mail inválido." })
+  @IsNotEmpty({ message: "Preencha o campo 'E-mail'." })
+  email: string;
+
+  @ApiProperty({
+    description:
+      "Senha de acesso (mínimo 8 caracteres, 1 número e 1 caractere especial)",
+    example: "senha!Forte123",
+  })
+  @IsString({ message: "Senha inválida." })
+  @MinLength(8, { message: "Senha deve ter 8 ou mais caracteres." })
+  @Matches(/.*\d/, { message: "Senha deve conter pelo menos um número." })
+  @Matches(/.*[!@#$%^&*(),.?":{}|<>]/, {
+    message: "Senha deve conter pelo menos um caractere especial.",
+  })
+  @IsNotEmpty({ message: "Preencha o campo 'Senha'." })
+  senha: string;
+
+  @ApiProperty({
+    description: "Tipo de usuário que será autenticado (entregador ou empresa)",
+    example: "entregador",
+  })
+  @IsEnum(TipoUsuario, {
+    message: 'O tipo de usuário deve ser "empresa" ou "entregador".',
+  })
+  @IsNotEmpty({ message: "O tipo de usuário é obrigatório." })
+  tipo: TipoUsuario;
+}
