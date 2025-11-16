d3085f2c45de79906cee89c90f1f954cfdc1084e
Caio-C8 <caiomgf50@gmail.com>
2025-11-16 15:16:58 -0300

    atualiza componentes principais para utilizar o layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,103 +1,55 @@
 import React, { useState } from "react";
-import Input from "./components/ui/Input/Input";
-import BotaoTexto from "./components/ui/Botao/BotaoTexto";
-import { FaUser, FaEnvelope, FaIdCard } from "react-icons/fa";
+import Header from "./components/layout/Header/Header";
+import SideBar from "./components/layout/SideBar/SideBar";
+import { Routes, Route, useLocation } from "react-router-dom";
 
-function App() {
-  const [nome, setNome] = useState("");
-  const [email, setEmail] = useState("");
-  const [cpf, setCpf] = useState("");
+const getTitulo = (path: string): string => {
+  switch (path) {
+    case "/":
+      return "Início";
+    case "/adicionar-saldo":
+      return "Adicionar Saldo";
+    case "/historico":
+      return "Histórico";
+    case "/solicitar-entrega":
+      return "Solicitar Entrega";
+    case "/perfil":
+      return "Perfil";
+    default:
+      return "Início";
+  }
+};
 
-  const [mostrarIconeNome, setMostrarIconeNome] = useState(true);
-  const [ativoNome, setAtivoNome] = useState(false);
-
-  const [mostrarIconeEmail, setMostrarIconeEmail] = useState(true);
-  const [ativoEmail, setAtivoEmail] = useState(false);
-
-  const [mostrarIconeCpf, setMostrarIconeCpf] = useState(true);
-  const [ativoCpf, setAtivoCpf] = useState(false);
-
-  const [senha, setSenha] = useState("");
-  const [mostrarSenha, setMostrarSenha] = useState(false);
-
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    alert(`Nome: ${nome}\nEmail: ${email}\nCPF: ${cpf}`);
-
-    setMostrarIconeNome(true);
-    setAtivoNome(false);
-
-    setMostrarIconeEmail(true);
-    setAtivoEmail(false);
-
-    setMostrarIconeCpf(true);
-    setAtivoCpf(false);
-  };
+const App: React.FC = () => {
+  const [isSideBarExpandida, setIsSideBarExpandida] = useState(false);
+  const location = useLocation();
+  const tituloDinamico = getTitulo(location.pathname);
+  const isPerfil = location.pathname === "/perfil";
 
   return (
-    <div style={{ padding: "40px", maxWidth: "500px", margin: "0 auto" }}>
-      <h1>Meu Formulário</h1>
-      <form onSubmit={handleSubmit}>
-        <Input
-          label="Nome"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-          placeholder="Digite seu nome"
-          Icon={FaUser}
-          iconPosition="right"
-          mostrarIcone={mostrarIconeNome}
-          desativado={!ativoNome}
-          onIconClick={() => {
-            setAtivoNome(true);
-            setMostrarIconeNome(false);
-          }}
-        />
-        <Input
-          label="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Digite seu email"
-          Icon={FaEnvelope}
-          iconPosition="right"
-          mostrarIcone={mostrarIconeEmail}
-          desativado={!ativoEmail}
-          onIconClick={() => {
-            setAtivoEmail(true);
-            setMostrarIconeEmail(false);
-          }}
-        />
-        <Input
-          label="CPF"
-          value={cpf}
-          onChange={(e) => setCpf(e.target.value)}
-          placeholder="Digite seu CPF"
-          Icon={FaIdCard}
-          iconPosition="right"
-          mostrarIcone={mostrarIconeCpf}
-          desativado={!ativoCpf}
-          onIconClick={() => {
-            setAtivoCpf(true);
-            setMostrarIconeCpf(false);
-          }}
-        />
-        <Input
-          label="Senha"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-          placeholder="Digite sua senha"
-          Icon={FaIdCard}
-          iconPosition="right"
-          desativado={false}
-          mostrarIcone={true}
-          type={mostrarSenha ? "text" : "password"}
-          onIconClick={() => setMostrarSenha(!mostrarSenha)}
-        />
+    <>
+      <SideBar
+        isExpandido={isSideBarExpandida}
+        setIsExpandido={setIsSideBarExpandida}
+      />
 
-        <BotaoTexto texto="Enviar" type="submit" />
-      </form>
-    </div>
+      <main>
+        <Header titulo={tituloDinamico} isPerfil={isPerfil} />
+
+        <Routes>
+          <Route path="/" />
+          <Route path="/adicionar-saldo" />
+          <Route path="/historico" />
+          <Route path="/solicitar-entrega" />
+          <Route path="/perfil" />
+        </Routes>
+      </main>
+
+      {isSideBarExpandida && (
+        <div className="overlay" onClick={() => setIsSideBarExpandida(false)} />
+      )}
+    </>
   );
-}
+};
 
 export default App;
